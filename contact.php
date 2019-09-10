e905ec2f7187906c64aaded2baa9d360f8de9d84
Mary <maryisnotgrumpy@gmail.com>
2019-09-10 11:23:51 -0400

    Homework 1 Complete

--- contact.php
+++ contact.php
@@ -1,13 +1,43 @@
-<?php
-/**
-@category   homework
-@package    contact.php
-@author     Mary Henry
-@version    9.5.2019
-@link
-@see
-*/
+<!doctype html>
 
-echo "Thwork 1";
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Contact Mary H</title>
+    <meta name="homework1">
+
+</head>
+
+<body>
+<center>
+    <?php
+    /**
+    @category   homework
+    @package    contact.php
+    @author     Mary Henry
+    @version    9.5.2019
+    @link       https://cislinux.hfcc.edu/~mehenry/contact.php
+    @see
+     */
+
+    echo "<h1>Mary H</h1>";
+
+    $email = "maryisnotgrumpy@gmail.com";
+
+    echo "<p>My e-mail = $email</p>";
+
+    define ('SLACK', 'Mary Henry');
+
+    echo "<p>My slack username = " . SLACK;
 
 
+
+
+    ?>
+</center>
+
+
+
+</body>
+</html>
