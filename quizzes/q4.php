a8a05d5b209c319b2f8d9cdea44854413e5afbb6
Chad Banks <chadbanks@quickenloans.com>
2019-10-08 16:08:11 -0400

    Graded quiz 4.

--- quizzes/q4.php
+++ quizzes/q4.php
@@ -8,11 +8,11 @@
  * @package     Quiz 4
  * @author      mary henry <mehenry@hfcc.edu>
  * @version     2019.10.03
- * @grade
+ * @grade       7 / 10
  */
 
 /**
- * 3 pts
+ * 2/3 pts
  * 1. You are working on a new project with a high profile client
  *  It is your job to set up a database connection.
  *  Create a variable called $mysqlconnection and store a new database connection object in it.
@@ -25,33 +25,33 @@ $DBHOST = 'robobook.com';
 $DBNAME = 'robobook_prod';
 $DBCHARSET = 'utf8';
 
-
+// Var names
 $dbc = mysqli_connect($servername, $username, $password);
 
 
 
 /**
- * 3 pts
+ * 2/3 pts
  * 2. Use the previous connection object to select all columns from a table called memposts.
  *	Once you execute this, loop through the results and echo a column called `mptxt`.
  *	Assume there are a total of 7 columns and the `mempost_id` field is the primary key.
  *	Feel free to ask about the table structure at any time during this quiz if needed.
  */
-$sql = "SELECT  FROM robobook_prod";
+$sql = "SELECT  FROM memposts";
 $result = mysqli_query($conn, $sql);
 
 
 /**
- * 2 pts
+ * 1/2 pts
  * 3. Use this connection to execute an update on the previous table.
  *	You want to set a field called `hidden` to 1 and `updated` to the current date and time.
  *	Only do this for all rows that have a `user_id` of 4 or 7.
  */
 $sql = "UPDATE robobook_prod SET hidden='1', updated='Now()' WHERE id=4,7";
-
+// execute?
 
 /**
- * 2 pts
+ * 1/2 pts
  * 4. Manually insert a row into this table.
  *	You only need to specify the following fields: `mempost_id`, `user_id`, `mptxt`, `created`
  *	Assume all unspecified fields will default to their values.
@@ -60,11 +60,11 @@ $sql = "UPDATE robobook_prod SET hidden='1', updated='Now()' WHERE id=4,7";
  *	The message should read 'I love PHP!' without the quotes.
  */$sql = "INSERT INTO robobook_prod ('mempost_id', 'user_id', 'mptxt', 'created')
 VALUES (null, 1337, i love php! , NOW() )";
-
+// execute?
 
 
 
 /**
- * 1 pt
+ * 1/1 pt
  * E. Explain some advantages of a PDO database connection over a mysqli connection.
  */The PDO connection is more secure. It allows you to use multiple database drivers.
