c2ccc8adde12514f54f75a22cc900cdba4bea3d0
Mary <maryisnotgrumpy@gmail.com>
2019-10-10 15:26:48 -0400

    commented out broke code

--- p1/index.php
+++ p1/index.php
@@ -54,7 +54,7 @@
      ?>
 
 	<main>
-        <div style="text-align: center;"><img src="images/cup.jpg" alt="flower teacup">
+        <div style="text-align: center; color: darkseagreen;"><img src="images/cup.jpg" alt="flower teacup">
             <h2>With a love of all things tiny and gardening I decided what better to do than incorporate those things into tiny treasures to share.</h2></div>
 	</main>
 
--- p1/store.php
+++ p1/store.php
@@ -19,31 +19,6 @@
     include 'head.php';
     ?>
 
-    <?php
-    /**
-     include_once "connect.php";
-
-     $dbh = new PDO("mysql:host=$host;dbname=$db",$user,$pass);
-     $result = $db->("SELECT `item_id`, `item_name`, `item_price`FROM product_records");
-    $result->execute($db);
-    ?>
-    <?php
-    foreach($result->fetch(PDO::FETCH_ASSOC) as $row) :
-    $dbh = null;
-    ?>
-
-    <table>
-    <tr>
-        <td><?php echo $row['item_id']; ?></td>
-        <td><?php echo $row['item_name']; ?></td>
-        <td><?php echo $row['item_price']; ?></td>
-    </tr>
-
-    </table>
-
-
-
-    */ ?>
 
 
 <body>
@@ -53,6 +28,53 @@
     ?>
 
 <main role="main">
+    <?php
+echo "<table style='border: solid 1px black; color: darkseagreen;align: center;'>";
+echo "<tr><th>Id</th><th>Product Name</th><th>Product Despcription</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;color:darkseagreen;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+
+$servername = "localhost";
+$db="mehenry";
+$username = "mehenry";
+$password = "samsvyzt";
+$charset='utf8mb4';
+
+
+try {
+    $dbh = new PDO("mysql:servername=$servername;dbname=$db", $username, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $dbh->prepare("SELECT item_id, item_name, item_desc FROM product_records");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
 </main>
 
     <?php
