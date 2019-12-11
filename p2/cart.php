1c28dfe1e327ae77a295bbf61a8a65d3cceccf69
Mary <maryisnotgrumpy@gmail.com>
2019-12-10 23:10:51 -0500

    project

--- p2/cart.php
+++ p2/cart.php
@@ -20,7 +20,7 @@ if (isset($_POST['action']) && $_POST['action']=="change"){
     foreach($_SESSION["shopping_cart"] as &$value){
         if($value['code'] === $_POST["code"]){
             $value['quantity'] = $_POST["quantity"];
-            break; // Stop the loop after we've found the product
+            break;
         }
     }
 
